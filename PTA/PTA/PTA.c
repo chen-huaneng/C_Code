9e65a051a76574bf34f898ce19d7a318c0143b9b
Abel <113616701+A-Le-Tian@users.noreply.github.com>
2022-09-24 11:51:44 +0800

    Add files via upload
    初学C语言的相关知识

---
+++ PTA/PTA/PTA.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//
+//    printf("计算乘法公式：\n");
+//    printf("%d",3*90);
+//
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//
+//    printf("hello world!");
+//
+//        return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("This Program prints a formula :\n72*88=%d",72*88);
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+    int x,y,z;
+
+    x = 30;
+
+    y = 60;
+
+    z = x;
+
+    x = y;
+
+    y = z;
+
+    printf("%d\n%d\n", x, y);
+
+    return 0;
+}
